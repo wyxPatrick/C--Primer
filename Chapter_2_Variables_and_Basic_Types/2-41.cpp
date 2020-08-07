92f62a2f90cf33d2da69df3df9bafb3e92542be0
Youxing Wang <wyxpatrick@gmail.com>
2020-08-07 16:26:45 -0400

    2.6 exercises

---
+++ Chapter_2_Variables_and_Basic_Types/2-41.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <string>
+
+struct Sales_data {
+    std::string bookNo;
+    unsigned units_sold = 0;
+    double revenue = 0.0;
+};
+
+int main() {
+    Sales_data total, current;
+    double price = 0;
+    if (std::cin >> total.bookNo >> total.units_sold >> price) {
+        int cnt = 1;
+        total.revenue = total.units_sold * price;
+        while (std::cin >> current.bookNo >> current.units_sold >> price) {
+            current.revenue = current.units_sold * price;
+            if (total.bookNo == current.bookNo) {
+                total.units_sold += current.units_sold;
+                total.revenue += current.revenue;
+                ++cnt;
+            }
+            else {
+                std::cout << total.bookNo << " " << cnt << std::endl;
+                total.bookNo = current.bookNo;
+                total.units_sold = current.units_sold;
+                cnt = 1;
+            }
+        }
+        std::cout << total.bookNo << " " << cnt << std::endl;
+    }
+    else {
+        std::cerr << "Data must refer to the same ISBN" << std::endl;
+        return -1;
+    }
+    return 0;
+}
